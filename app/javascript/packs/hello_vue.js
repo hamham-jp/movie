27ac7de5343062fd053cf0794abeeaf992ab194b
浜口裕介 <>
2020-05-10 11:47:33 +0900

    vuetify icon使える様に

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,9 +9,14 @@ import Vue from 'vue'
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import App from '../app.vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify);
-const vuetify = new Vuetify();
+const vuetify = new Vuetify({
+  icons: {
+    iconfont: 'mdi'
+  }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
