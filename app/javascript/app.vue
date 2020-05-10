bc9408ea7465d60e8f077d005f6f725dbefe1192
浜口裕介 <>
2020-05-10 11:55:53 +0900

    no message

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app id="app">
+    <!--メニューバー-->
     <template>
       <div>
         <v-app-bar
@@ -44,9 +45,66 @@
         </v-app-bar>
       </div>
     </template>
-    <div>
-      <p>{{ message }}</p>
-    </div>
+    <!--スライダー-->
+    <template>
+      <v-sheet
+        class="mx-auto"
+        elevation="8"
+        max-width="800"
+      >
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          show-arrows
+        >
+          <v-slide-item
+            v-for="n in 15"
+            :key="n"
+            v-slot:default="{ active, toggle }"
+          >
+            <v-card
+              :color="active ? 'primary' : 'grey lighten-1'"
+              class="ma-4"
+              height="200"
+              width="100"
+              @click="toggle"
+            >
+              <v-row
+                class="fill-height"
+                align="center"
+                justify="center"
+              >
+                <v-scale-transition>
+                  <v-icon
+                    v-if="active"
+                    color="white"
+                    size="48"
+                    v-text="'mdi-close-circle-outline'"
+                  ></v-icon>
+                </v-scale-transition>
+              </v-row>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+
+        <v-expand-transition>
+          <v-sheet
+            v-if="model != null"
+            color="grey lighten-4"
+            height="200"
+            tile
+          >
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <h3 class="title">Selected {{ model }}</h3>
+            </v-row>
+          </v-sheet>
+        </v-expand-transition>
+      </v-sheet>
+    </template>
   </v-app>
 </template>
 
@@ -54,7 +112,8 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      model: null,
     }
   }
 }
